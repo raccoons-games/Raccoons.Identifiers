16b585cc53757448b41375a1b55e92373f99dc3a
ProkusXD <64917745+ProkusXD@users.noreply.github.com>
2022-09-22 10:36:48 +0300

    RFT-43 Added base framework logic (#1)
    * RFT-44 Added base framework logic
    
    * RFT-43 Fixed grammatic
    
    * RFT-43 Fixed generic names
    
    * RFT-43 Fixed method grammar
    
    Co-authored-by: thevitalife <37376394+thevitalife@users.noreply.github.com>

---
+++ Runtime/Guids/IGuid.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Raccoons.Identifiers.Guids
+{
+    public interface IGuid
+    {
+        string Key { get; }
+    }
+}
